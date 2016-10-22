0f4185a06a0207233bc75d2645ac2c4de2c00ba1
Shtong <shtong@mm.st>
2016-10-22 09:17:29 +0200

    Removed the OpenID secret from a source file. Whip me.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate irc;
 mod checker;
 
 use std::default::Default;
+use std::fs::File;
+use std::io::{Result, Read};
 use irc::client::prelude::*;
 
 fn main() {
@@ -11,12 +13,12 @@ fn main() {
         port: Some(6667),
         channels: Some(vec![format!("#le_shtong")]),
         nickname: Some(format!("Purple_Hammer")),
-        password: Some(format!("oauth:78sfum5mlfghts0u0y8jqc6sg52ppx")),
+        password: Some(read_oauth_token().unwrap()),
         owners: Some(vec![format!("Le_Shtong")]),
         .. Default::default()
     };
 
-    println!("Connecting to IRC...");
+    println!("Connecting to IRC with token ...");
     let server = IrcServer::from_config(irc_config).unwrap();
     server.identify().unwrap();
     println!("Connected!");
@@ -39,4 +41,11 @@ fn main() {
             _ => (),
         }
     }
+}
+
+fn read_oauth_token() -> Result<String> {
+    let mut result = String::new();
+    let mut file = try!(File::open("twitch_openid.txt"));
+    try!(file.read_to_string(&mut result));
+    Ok(result)
 }
